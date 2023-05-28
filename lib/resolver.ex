c45e10fa2413bf9c3db4f1db61fc85dc2e2da84a
Jett Jackson <me@jett.dev>
2023-05-28 13:13:34 +0800

    feat: process authorities and additionals

--- lib/resolver.ex
+++ lib/resolver.ex
@@ -54,20 +54,30 @@ defmodule Resolver do
     defstruct [:name, :type, :class, :ttl, :data]
 
     def fromBitstring(body, answer) do
-      {name, <<type::16, 1::16, ttl::32, len::16, data::binary-size(len), remainder::binary>>} =
-        Resolver.Name.fromBitstring(body, answer)
+      {name, remainder} = Resolver.Name.fromBitstring(body, answer)
 
-      parsedData =
-        case type do
-          1 ->
-            Resolver.IP.fromBitstring(data)
+      IO.inspect(name, label: "name")
+      IO.inspect(remainder, label: "remainder")
 
-          5 ->
+      # <<type::16, 1::16, ttl::32, len::16, data::binary-size(len), remainder::binary>> = remainder
+      <<type::16, remainder::binary>> = remainder
+      # data = <<192, 168, 1, 1>>
+
+      {parsedData, ttl, remainder} =
+        case {type, remainder} do
+          {1, <<1::16, ttl::32, len::16, data::binary-size(len), remainder::binary>>} ->
+            {Resolver.IP.fromBitstring(data), ttl, remainder}
+
+          {5, <<1::16, ttl::32, len::16, data::binary-size(len), remainder::binary>>} ->
             {return, _} = Resolver.Name.fromBitstring(body, data)
-            Enum.join(return, ".")
+            {Enum.join(return, "."), ttl, remainder}
 
-          _ ->
-            {:unknowntype}
+          {2, <<1::16, ttl::32, len::16, data::binary-size(len), remainder::binary>>} ->
+            {return, _} = Resolver.Name.fromBitstring(body, data)
+            {Enum.join(return, "."), ttl, remainder}
+
+          {28, <<1::16, ttl::32, len::16, data::binary-size(len), remainder::binary>>} ->
+            {data, ttl, remainder}
         end
 
       {%Resolver.Answer{
@@ -96,6 +106,17 @@ defmodule Resolver do
     def fromBitstring(body, nameRaw, accumulator \\ [])
 
     def fromBitstring(
+          body,
+          <<1::1, 1::1, pointer::14, remainder::binary>>,
+          accumulator
+        ) do
+      IO.inspect(pointer, label: "dns compression pointer")
+      <<_::binary-size(pointer - 12), nameRaw::binary>> = body
+      {name, _} = fromBitstring(body, nameRaw)
+      {accumulator ++ name, remainder}
+    end
+
+    def fromBitstring(
           _body,
           <<0::8, remainder::binary>>,
           accumulator
@@ -108,17 +129,9 @@ defmodule Resolver do
           <<len::8, domainParts::binary-size(len), remainder::binary>>,
           accumulator
         ) do
+      IO.inspect(domainParts, label: "domainparts")
+      IO.inspect(len, label: "domain part length")
       fromBitstring(body, remainder, accumulator ++ [domainParts])
-    end
-
-    def fromBitstring(
-          body,
-          <<1::1, 1::1, pointer::14, remainder::binary>>,
-          accumulator
-        ) do
-      <<_::binary-size(pointer - 12), nameRaw::binary>> = body
-      {name, _} = fromBitstring(body, nameRaw)
-      {accumulator ++ name, remainder}
     end
   end
 
@@ -134,32 +147,65 @@ defmodule Resolver do
   end
 
   defp unpackDNSResponse(<<headerRaw::binary-size(12), body::binary>>) do
+    IO.puts("unpacking header")
     header = Header.fromBitstring(headerRaw)
 
-    {question, answerRaw} = Question.fromBitstring(body)
+    IO.puts("unpacking question")
+    {question, data} = Question.fromBitstring(body)
 
-    {answers, _} =
-      Enum.reduce(1..header.num_answers, {[], answerRaw}, fn _, {answers, remainder} ->
-        {answer, answerRaw} = Answer.fromBitstring(body, remainder)
-        {[answer | answers], answerRaw}
-      end)
+    IO.inspect(header, label: "header")
 
-    {header, question, answers}
+    # TODO: bug, enum is called multiple times even when header.num_answers is 0
+    {answers, remainder} =
+      if header.num_answers > 0 do
+        Enum.reduce(1..header.num_answers, {[], data}, fn _, {answers, remainder} ->
+          IO.puts("enumerating answer")
+          {answer, remainder} = Answer.fromBitstring(body, remainder)
+          {[answer | answers], remainder}
+        end)
+      else
+        {nil, data}
+      end
+
+    {authorities, additionalsRaw} =
+      if header.num_authorities > 0 do
+        Enum.reduce(1..header.num_authorities, {[], remainder}, fn _, {answers, remainder} ->
+          IO.puts("enumerating authorities")
+          {answer, remainder} = Answer.fromBitstring(body, remainder)
+          {[answer | answers], remainder}
+        end)
+      else
+        {nil, remainder}
+      end
+
+    {additionals, _} =
+      if header.num_additionals > 0 do
+        Enum.reduce(1..header.num_additionals, {[], additionalsRaw}, fn _, {answers, remainder} ->
+          IO.puts("enumerating additionals")
+          {answer, remainder} = Answer.fromBitstring(body, remainder)
+          {[answer | answers], remainder}
+        end)
+      else
+        {nil, nil}
+      end
+
+    {header, question, answers, authorities, additionals}
   end
 
   defp buildQuery(name, recordType) do
     name = Name.toBitstring(name)
     id = :rand.uniform(65535)
-    flags = <<1 <<< 8::16>>
+    flags = <<0 <<< 8::16>>
+    # flags = <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0>>
     header = Header.toBitstring(%Header{id: id, flags: flags, num_questions: 1})
     question = Question.toBitstring(%Question{name: name, type: recordType, class: 1})
     header <> question
   end
 
-  def query(domain) do
+  def query(domain, resolver) do
     dnsPacket = buildQuery(domain, 1)
     {:ok, socket} = :gen_udp.open(0, [:binary, active: false])
-    :ok = :gen_udp.send(socket, {8, 8, 8, 8}, 53, dnsPacket)
+    :ok = :gen_udp.send(socket, resolver, 53, dnsPacket)
 
     return =
       with {:ok, {_address, _port, data}} <- :gen_udp.recv(socket, 1024, :infinity) do
